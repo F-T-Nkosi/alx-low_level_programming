adb0f57e1d60567f9c57a2e9f85614429f566df4
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-19 12:14:44 -0700

    0x02 functions nested loops task 0

---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+programms included in this project
+0-putchar.c prints _putchar
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,10 @@
+#include<unistd.h>
+
+/**
+ * main - writes the character c to stsout
+ * @c: The charcater to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
