72b6ad46a6aef13ea98ced4ea7330be87c452a1b
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-20 10:49:21 -0700

    task 5 0x02 functions nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_sign - writes the sign of an input number
+ * @n: is the number evaluated
+ *
+ * Return: 1 for positive numbers, 0 for 0 and -1 for negative
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+		_putcar('-');
+		return (-1);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,4 +4,5 @@ programms included in this project
 2-print_alphabet_x10.c prints alphabet in lowercase 10 times
 3-islower.c checks for lower case and returns 1 if true and 0 if not
 4-isalpha.c checks if a character is a letter
+5-sign.c prints the sign of a number
 
