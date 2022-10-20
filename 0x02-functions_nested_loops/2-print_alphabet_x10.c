918c97167236936fa326b26ff58530b22acb29c6
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-20 04:12:35 -0700

    0x02 functions nested loops task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - writes the alphabet in lowercase x10
+ *
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+
+	int b = 1;
+
+	while (b <= 10)
+	{
+		while (a <= 'z')
+		{
+			_putchar(a);
+			if (a == 'z')
+				putchar('\n');
+			a++;
+		}
+		b++;
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,4 +1,5 @@
 programms included in this project
-0-putchar.c prints _putchar
+0i-putchar.c prints _putchar
 1-alphabet.c function prints the alphabet in lower case 
+2-print_alphabet_x10.c prints alphabet in lowercase 10 times
 
