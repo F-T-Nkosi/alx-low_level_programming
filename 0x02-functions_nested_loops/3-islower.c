2a684c02042cbe5304bdd3d48e12e37861086989
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-20 07:24:41 -0700

    task3 hf 2

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,12 +3,13 @@
 
 /**
  * _islower - writes if a character is lower or not
+ *@c: The character evaluated
  *
  * Return: Always 1 if c is lower, else 0
  */
 int _islower(int c)
 {
-	if (c <= 57)
+	if (c <= 97 && c <= 122)
 	{
 		return (1);
 	}
