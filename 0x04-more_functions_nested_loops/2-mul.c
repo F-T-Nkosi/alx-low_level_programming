b8c6163cb4578859dc5e569de6ec42ab6a3228d5
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-21 11:45:46 -0700

    callaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ * mul - multiplies two inputs
+ * @a: is the first num input
+ * @b: is the second num input
+ * Return: result of a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 C programming project 4
 0-isupper.c is a function that checks for uppercase characters
 1-isdigit.c function checks if a character is a digit
+2-mul.c function multiplies two inputs
