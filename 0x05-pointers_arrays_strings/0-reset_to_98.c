edff9b5b5995de74e576eaf01435e29e81aa4208
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-24 11:10:41 -0700

    98 Bettery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+/**
+ * reset_to_98 - updates the value that a pointer points to
+ * @n: is a pointer pointing to the variable we set to 98
+ * Return: Always 0 (Sucess)
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
