2ec3aa92e32688d10efcb301b19d71a9575d79d2
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-10-24 17:50:24 -0700

    hot fix

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -25,13 +25,13 @@ int _atoi(char *s)
 			++j;
 		if (s[i] >= '0' && s[i] <= '9')
 			num = s[i] - '0';
-			if (j % 2)
-				num = -num;
-			con = con * 10 + num;
-			test = 1;
-			if ((s[i + 1] < '0') || (s[i + 1] > '9'))
-				break;
-			test = 0;
+		if (j % 2)
+			num = -num;
+		con = con * 10 + num;
+		test = 1;
+		if ((s[i + 1] < '0') || (s[i + 1] > '9'))
+			break;
+		test = 0;
 	}
 		i++;
 	if (test == 0)
