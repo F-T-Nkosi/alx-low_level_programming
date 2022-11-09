8d9f788b11a20ae22a5679af8fa0c204d5926dcd
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-11-09 10:49:15 -0800

    Other means

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - prints number of arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: number of arguments
+ */
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,3 @@
 0x0A-argc_argv Project
 0-whatsmyname.c prints its own name 
+1-args.c prints the number of aguments passed to it
