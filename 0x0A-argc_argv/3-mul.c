27a1bf48c9ccc2ec28cd94ce2bc59379f4b2bb9a
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-11-09 11:29:09 -0800

    Task 3 Neither irony nor sarcasm

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - prints the product of two numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: Always 0 for success else 1
+ */
+int main(int argc, char *argv[])
+{
+	if (argc <= 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	return (0);
+}
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -2,3 +2,4 @@
 0-whatsmyname.c prints its own name 
 1-args.c prints the number of aguments passed to it
 2-args.c prints all arguments it recieves
+3-mul.c prints the product of two numbers
