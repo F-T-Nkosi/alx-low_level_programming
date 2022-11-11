e4a97e1324eba352c1a04ec9d9060484c39a96d1
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-11-11 05:27:21 -0800

    task 4 hot fix

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -14,13 +14,13 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	unsigned int i;
 
 	if (new_size == old_size)
-	{
 		free(ptr);
 		return (NULL);
-	}
+	if (new_size == 0 && ptr)
+		free(ptr);
+		return (NULL);
 	if (!ptr)
 		return (malloc(new_size));
-
 	nptr = malloc(new_size);
 	if (!nptr)
 		return (NULL);
