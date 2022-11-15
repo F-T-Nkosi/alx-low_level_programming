910bb5d658cd0ffe7d1bac756ae326c9725dc037
Thamsanqa F Nkosi <nkosijnr2nd.tn@gmail.com>
2022-11-15 08:37:43 -0800

    Task 2 file name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints name of file
+ * 
+ * Return: Always 0
+ */
+int main()
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
--- 0x0D-preprocessor/README.md
+++ 0x0D-preprocessor/README.md
@@ -1,2 +1,3 @@
 0x0D-preprocessor Project
 0-object_like_macro.h define macro SIZE as 1024
+2-main.c prints the name of the file it was compiled in
